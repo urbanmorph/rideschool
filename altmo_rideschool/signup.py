4b50bcf7b2a9dc4ee3196cae909c04f083daa34f
nivurban <NIVYA@DESKTOP-S7KTG2G.(none)>
2024-01-10 01:34:54 +0530

    alert and chaned the return messages that are displayed inside the alert

--- altmo_rideschool/signup.py
+++ altmo_rideschool/signup.py
@@ -1,30 +1,15 @@
 print("signup.py")
 
-
 import bcrypt
-#import psycopg2
 import traceback
-#import os
 import logging
-#from base64 import urlsafe_b64encode
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, Length
 from altmo_utils.db import get_db_cursor
-#from .config.config import get_config_value 
 
 signup_bp = Blueprint('signup', __name__)
-
-#  config file , pool
-# PostgreSQL database connection
-##db_connection = psycopg2.connect(
-  ##  host=get_config_value('db_host'),
-    ##database=get_config_value('db_name'),
-    ##user=get_config_value('db_user'),
-    ##password=get_config_value('db_password')
-##)
-##db_cursor = db_connection.cursor()
 
 # Define a form for the signup page
 class SignupForm(FlaskForm):
@@ -37,8 +22,6 @@ class SignupForm(FlaskForm):
 def index():
     form = SignupForm()
     return render_template('signup.html', form=form)
-
-
 
 @signup_bp.route('/check_contact', methods=['POST'])
 def check_contact():
@@ -54,7 +37,7 @@ def check_contact():
                 existing_user = db_cursor.fetchone()
 
             if existing_user:
-                return jsonify(alert_type='danger', message=f"The contact number {contact} is already registered.")
+                return jsonify(alert_type='danger', message=f"An account with the provided contact number {contact} already exists. Kindly proceed to login.")
 
             # Check if the contact is in the participants table
             with get_db_cursor() as db_cursor:
@@ -74,8 +57,8 @@ def check_contact():
                 hashed_password = hash_password(user_password)
                 with get_db_cursor(commit=True) as db_cursor:
                     db_cursor.execute("INSERT INTO users_signup (contact_no, password, role) VALUES (%s, %s, %s)", (contact, hashed_password, role))
-
-                return jsonify(alert_type='success', message=f"Hello {role.capitalize()}, you have created an account successfully.")
+                #return jsonify(alert_type='success', message="Your account has been created successfully.")
+                return jsonify(alert_type='success', message=f"Hello {role.capitalize()}, Your account has been created successfully.")
 
             # Check if the contact is in the trainer table
             with get_db_cursor() as db_cursor:
@@ -127,8 +110,7 @@ def check_contact():
         error_message = f"An error occurred while processing the signup.{str(e)}"
         return jsonify(alert_type='danger', message=error_message)
 
-    #finally:
-        #db_connection.close()
+
 def hash_password(password):
     # Generate a random salt using bcrypt
     
