aff439b43d5778adc2907c9a975beb55ab1b67ff
nivurban <NIVYA@DESKTOP-S7KTG2G.(none)>
2024-01-10 12:12:28 +0530

    alert

--- altmo_rideschool/organization.py
+++ altmo_rideschool/organization.py
@@ -1,12 +1,10 @@
 # organization.py
-
-print("import feane/ organization.py")
+print("organization.py")
 import traceback
 from flask import Blueprint, render_template, request, current_app, send_from_directory, jsonify
 import os
 import uuid
 from werkzeug.utils import secure_filename
-#from .db import get_db_cursor
 from altmo_utils.db import get_db_cursor
 
 organization_bp = Blueprint('organization', __name__)
@@ -28,60 +26,28 @@ def submit_organization():
         coordinator_name = request.form['coordinator-name']
         coordinator_email = request.form['coordinator-email']
         coordinator_contact = request.form['coordinator-contact']
-
         # Handle file upload
-        #legal_status_document = request.files['organization-legal-status-document']
-        #file_path = None
-
-        #if legal_status_document:
-            # Generate a unique filename using UUID and save to the upload folder
-         #   filename = str(uuid.uuid4()) + secure_filename(legal_status_document.filename)
-          #  file_path = os.path.join(current_app.config['ORGANIZATION_FOLDER'], filename)
-           # legal_status_document.save(file_path)
-        #if legal_status_document:
-            # Ensure the directory exists
-            #if not os.path.exists(current_app.config['ORGANIZATION_FOLDER']):
-               # os.makedirs(current_app.config['ORGANIZATION_FOLDER'])
-
-            # Generate a unique filename using UUID and save to the upload folder
-            #filename = str(uuid.uuid4()) + secure_filename(legal_status_document.filename)
-            #file_path = os.path.join(current_app.config['ORGANIZATION_FOLDER'], filename)
-            ##file_path = os.path.join(current_app.config['ORGANIZATION_FOLDER'], filename.replace('/', '\\'))
-            # Print the generated file path for debugging
-           # file_path = os.path.join(current_app.config['ORGANIZATION_FOLDER'], filename)
-            #file_path = file_path.replace('/', os.path.sep)
-           ## print("Generated file path:", file_path)
-
-            #legal_status_document.save(file_path)
-        
         organization_legal_status_document = request.files['organization-legal-status-document']
         if organization_legal_status_document:
             document_filename = secure_filename(organization_legal_status_document.filename)
             relative_document_path = os.path.join('static', 'organization_image', document_filename)
             document_path_full = os.path.join(current_app.root_path, current_app.config['ORGANIZATION_FOLDER'], document_filename)
 
-    # Ensure the directory exists
+            # Ensure the directory exists
             os.makedirs(os.path.dirname(document_path_full), exist_ok=True)
 
-    # Save the document
+            # Save the document
             organization_legal_status_document.save(document_path_full)
 
-
-        # Perform database insertion or any other necessary operations
+            # Perform database insertion or any other necessary operations
             with get_db_cursor(commit=True) as cursor:
-
                 # check if contact no is already registered 
                 chech_contact = "SELECT organisation_id FROM organisation WHERE organisation_contact = %s"
                 cursor.execute(chech_contact, (organization_contact,))
                 existing_org = cursor.fetchone()
-    
-                
+                    
                 if existing_org:
-                
-                    print("Organization is already registered.")
-                    return jsonify({"status": "error", "message": "Organization with this contact number is already registered."})
-
-
+                    return jsonify({"alert_type ": "error", "message": "Organization with this contact number is already registered."})
 
                 cursor.execute("""
                 INSERT INTO public.organisation (
@@ -110,27 +76,17 @@ def submit_organization():
             ))
 
         # Display success message
-        message = f"Organization {organization_name} submitted successfully!"
-        return jsonify({'status': 'success', 'message': message})
-        #return "submitted successfully!"
-
+        message = f"{organization_name} has been registered successfully!"
+        return jsonify({'alert_type ': 'success', 'message': message})
+      
     except Exception as e:
         traceback.print_exc()
         # Log the exception for debugging purposes
-        print(f"An error occurred: {e}")
-
+        current_app.logger.error(f"An error occurred: {e}")
+ 
         # Display error message
-        return jsonify({'status': 'error', 'message': 'An error occurred. Please try again.'})
+        return jsonify({'alert_type ': 'error', 'message': 'An error occurred. Please try again.'})
 
-
-#@organization_bp.route('/organization_image/<filename>')
-#def display_image(filename):
- #   return send_from_directory(current_app.config['ORGANIZATION_FOLDER'], filename)
-
-##"D:\job\urban_morph\pedal_shaale_project\altmo_rideschool\static\organization_image"
-#@organization_bp.route('/display_legal_document/<filename>')
-#def display_legal_document(filename):
-   
  #   return send_from_directory(current_app.config['ORGANIZATION_FOLDER'], filename)
 @organization_bp.route('/display_legal_document/<filename>')
 def display_legal_document(filename):
