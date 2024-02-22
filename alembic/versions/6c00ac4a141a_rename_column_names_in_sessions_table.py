5f4cf90f1cedd384d44a7a2fc62c8ab6256e68ff
nivurban <NIVYA@DESKTOP-S7KTG2G.(none)>
2024-02-22 15:32:00 +0530

    rename the columns in the sessions table

--- alembic/versions/6c00ac4a141a_rename_column_names_in_sessions_table.py
+++ alembic/versions/6c00ac4a141a_rename_column_names_in_sessions_table.py
@@ -17,65 +17,66 @@ down_revision: Union[str, None] = 'e490608cd68c'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-
 def upgrade() -> None:
-    # Rename columns
-    op.alter_column('sessions', 'scheduled_datetime', new_columnc_name='scheduled_date')
-    op.alter_column('sessions', 'actual_datetime', new_column_name='actual_date')
-    op.alter_column('sessions', 'session_current_date', new_column_name='created_date')
-    op.alter_column('sessions', 'session_update_date', new_column_name='update_date')
-    op.alter_column('sessions', 'session_status', new_column_name='status')
+    with op.batch_alter_table('sessions') as batch_op:
+        # rename columns
+        batch_op.alter_column('scheduled_datetime', new_column_name='scheduled_date')
+        batch_op.alter_column('actual_datetime', new_column_name='actual_date')
+        batch_op.alter_column('session_current_date', new_column_name='created_date')
+        batch_op.alter_column('session_update_date', new_column_name='update_date')
+        batch_op.alter_column('session_status', new_column_name='status')
 
-    # Update trigger function name
-    op.execute("DROP TRIGGER IF EXISTS update_session_update_date ON public.sessions")
-    op.execute('''
-        CREATE OR REPLACE FUNCTION public.update_date()
-        RETURNS TRIGGER AS $$
-        BEGIN
-            NEW.update_date := CURRENT_TIMESTAMP;
-            RETURN NEW;
-        END;
-        $$ LANGUAGE plpgsql;
+        # update trigger function name
+        batch_op.execute("DROP TRIGGER IF EXISTS update_session_update_date ON public.sessions")
+        batch_op.execute('''
+            CREATE OR REPLACE FUNCTION public.update_date()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.update_date := CURRENT_TIMESTAMP;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
 
-        CREATE TRIGGER update_date
-        BEFORE UPDATE 
-        ON public.sessions
-        FOR EACH ROW
-        EXECUTE FUNCTION public.update_date();
-    ''')
+            CREATE TRIGGER update_date
+            BEFORE UPDATE 
+            ON public.sessions
+            FOR EACH ROW
+            EXECUTE FUNCTION public.update_date();
+        ''')
 
-    # Add foreign key constraints
-    op.create_foreign_key('fk_sessions_trainer_id', 'sessions', 'trainer', ['trainer_id'], ['id'])
-    op.create_foreign_key('fk_sessions_participant_id', 'sessions', 'participants', ['participant_id'], ['id'])
-
+        # add foreign key constraints
+        batch_op.create_foreign_key('fk_sessions_trainer_id', 'trainer', ['trainer_id'], ['id'])
+        batch_op.create_foreign_key('fk_sessions_participant_id', 'participants', ['participant_id'], ['id'])
 
 
 def downgrade() -> None:
-    # Drop foreign key constraints
-    op.drop_constraint('fk_sessions_trainer_id', 'sessions', type_='foreignkey')
-    op.drop_constraint('fk_sessions_participant_id', 'sessions', type_='foreignkey')
+    with op.batch_alter_table('sessions') as batch_op:
+        # Drop foreign key constraints
+        batch_op.drop_constraint('fk_sessions_trainer_id', type_='foreignkey')
+        batch_op.drop_constraint('fk_sessions_participant_id', type_='foreignkey')
 
-    # Rename columns its to original names
-    op.alter_column('sessions', 'created_date', new_column_name='session_current_date')
-    op.alter_column('sessions', 'update_date', new_column_name='session_update_date')
-    op.alter_column('sessions', 'status', new_column_name='session_status')
-    op.alter_column('sessions', 'scheduled_date',new_columnc_name='scheduled_datetime')
-    op.alter_column('sessions', 'actual_date', new_column_name='actual_datetime')
+        # rename columns back to their original names
+        batch_op.alter_column('created_date', new_column_name='session_current_date')
+        batch_op.alter_column('update_date', new_column_name='session_update_date')
+        batch_op.alter_column('status', new_column_name='session_status')
+        batch_op.alter_column('scheduled_date', new_column_name='scheduled_datetime')
+        batch_op.alter_column('actual_date', new_column_name='actual_datetime')
 
-    # Update trigger function name back to original
-    op.execute("DROP TRIGGER IF EXISTS update_date ON public.sessions")
-    op.execute('''
-        CREATE OR REPLACE FUNCTION public.update_session_update_date()
-        RETURNS TRIGGER AS $$
-        BEGIN
-            NEW.session_update_date := CURRENT_TIMESTAMP;
-            RETURN NEW;
-        END;
-        $$ LANGUAGE plpgsql;
+        # update trigger function name back to original triger 
+        batch_op.execute("DROP TRIGGER IF EXISTS update_date ON public.sessions")
+        batch_op.execute('''
+            CREATE OR REPLACE FUNCTION public.update_session_update_date()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.session_update_date := CURRENT_TIMESTAMP;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
 
-        CREATE TRIGGER update_session_update_date
-        BEFORE UPDATE 
-        ON public.sessions
-        FOR EACH ROW
-        EXECUTE FUNCTION public.update_session_update_date();
-    ''')
+            CREATE TRIGGER update_session_update_date
+            BEFORE UPDATE 
+            ON public.sessions
+            FOR EACH ROW
+            EXECUTE FUNCTION public.update_session_update_date();
+        ''')
+
