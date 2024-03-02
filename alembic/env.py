bfcc310f279cb9a47a0382675b9213a9dcd102da
nivurban <NIVYA@DESKTOP-S7KTG2G.(none)>
2024-03-02 12:39:54 +0530

    alembic .env

--- alembic/env.py
+++ alembic/env.py
@@ -32,15 +32,13 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-metadata = MetaData()   # By adding metadata = MetaData() and target_metadata = metadata to your Alembic environment script (env.py), we ensuring that Alembic has access to the necessary MetaData object it needs for autogenerating migrations.
+metadata = MetaData()   # By adding metadata = MetaData() and target_metadata = metadata to the Alembic environment script (env.py), we ensuring that Alembic has access to the necessary MetaData object it needs for autogenerating migrations.
 target_metadata = metadata
-
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
