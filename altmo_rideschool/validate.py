3f1a09aaae9141b773311c989924c1e3dce6f3f0
nivurban <NIVYA@DESKTOP-S7KTG2G.(none)>
2024-04-25 13:26:40 +0530

    added a validate script to ensures that only users with a specific role (admin, trainer, participant) can access certain routes

---
+++ altmo_rideschool/validate.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from flask import session, redirect, url_for 
+
+def user_required(route_func):
+    @wraps(route_func)
+    def decorated_route(*args, **kwargs):
+        if session.get('role') != 'participant':
+            error_message = 'Please login as a valid user to view this page.'
+            return redirect(url_for('logins.index', error_message=error_message))
+        return route_func(*args, **kwargs)
+    return decorated_route
+
+def trainer_required(route_func):
+    @wraps(route_func)
+    def decorated_route(*args, **kwargs):
+        if session.get('role') != 'trainer':
+            error_message = 'Please login as a valid user to view this page.'
+            return redirect(url_for('logins.index', error_message=error_message))
+        return route_func(*args, **kwargs)
+    return decorated_route
+
