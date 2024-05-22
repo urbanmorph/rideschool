58792f3a03100ad1f81972a06ddea46e9fbfafdc
Nivya98 <nivya@urbanmorph.com>
2024-05-22 16:50:12 +0530

    Add privacy note page to the application.

--- altmo_rideschool/__init__.py
+++ altmo_rideschool/__init__.py
@@ -1,9 +1,7 @@
 # init.py
-
 import os
 from flask import Flask, render_template
 from flask_session import Session
-
 from .admin import admin_bp
 from .logins import logins_bp
 from .participant import participant_bp
@@ -14,13 +12,10 @@ from .trainer import trainer_bp
 from .training_locations_list import training_locations_list_bp
 from .organization import organization_bp
 from .map import map_bp 
-
 from altmo_utils import db 
 import atexit # Close DB pool 
 import json 
 ##db.init
-
-# In the __init__.py, the create_app function sets up the Flask application
 
 _version_ = '1.0.0'
 
@@ -32,14 +27,14 @@ def create_app(test_config=None):
 
     if test_config is None:
         
-        app.config.from_file("config.json", load=json.load) # loading configuration settings from a config.json file and sets them in app.config. After this, we can access these settings using app.config['SETTING_NAME'] eg: app.config['ORGANIZATION_FOLDER'] = app.config.get('ORGANIZATION_FOLDER', 'static/organization_image').
+        app.config.from_file("config.json", load=json.load)
         
     else:
         app.config.from_mapping(test_config)
 
     app.secret_key = app.config.get('SECRET_KEY', 'default_secret_key')
 
-    print("SECRET_KEY:", app.secret_key)  # Print the SECRET_KEY for debugging
+    print("SECRET_KEY:", app.secret_key)  
 
     # Initialize Flask-Session after setting the secret key
     app.config['SESSION_TYPE'] = 'filesystem'
@@ -51,21 +46,19 @@ def create_app(test_config=None):
     # Enable CSRF protection
     app.config['CSRF_ENABLED'] = True
 
-   
     app.config['UPLOAD_FOLDER'] =  app.config.get('UPLOAD_FOLDER', 'static/uploaded_images')
     
     app.config['TRAINING_LOCATION_PICTURES_FOLDER'] = app.config.get('TRAINING_LOCATION_PICTURES_FOLDER', 'static/training_location_pictures')
    
-    app.config['ORGANIZATION_FOLDER'] = app.config.get('ORGANIZATION_FOLDER', 'static/organization_image') # thr r 2 reasons for this line 1. is "setting the default value" for 'ORGANIZATION_FOLDER' in the Flask app's configuration ,#2. this line "checks if 'ORGANIZATION_FOLDER' is present in the loaded configuration "(app.config). If it's present, it uses that value. If it's not present, it falls back to the default value 'static/organization_image' but this line is not compulsary the application will function correctly with or without this line.. 
+    app.config['ORGANIZATION_FOLDER'] = app.config.get('ORGANIZATION_FOLDER', 'static/organization_image') 
     
     #app.config['ORGANIZATION_FOLDER'] = 'static/organization_image'
 
     app.static_folder = 'static'
 
-    # Set DB_URL directly in app.config
     app.config["DB_URL"] = app.config.get("DATABASE_URI")
 
- # Initialize the database
+    # Initialize the database
     db.init_app(app)
     print("Database used in the project:", app.config.get("DB_URL"))
 
@@ -74,12 +67,14 @@ def create_app(test_config=None):
     def index():
         return render_template('index.html')
 
-    @app.route('/FAQ.html')
+    @app.route('/about')
     def about():
-        return render_template('FAQ.html')
+        return render_template('about.html')
+    
+    @app.route('/privacy_note.html')
+    def privacy_note():
+        return render_template('privacy_note.html')
   
-    # Register all the forms with the app
-   
     app.register_blueprint(participant_bp)
     app.register_blueprint(trainer_bp)
     app.register_blueprint(sessions_bp)
@@ -90,7 +85,7 @@ def create_app(test_config=None):
     app.register_blueprint(admin_bp)
     app.register_blueprint(organization_bp)
     app.register_blueprint(map_bp) 
-     # Close DB pool before exiting the app
+    # Close DB pool before exiting the app
     atexit.register(db.close_db_pool)
     return app
 
